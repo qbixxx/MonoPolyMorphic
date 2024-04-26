d23d497688cd765f5be61b5634b195779a83c688
Luciano Salerno <salerno.luciano@outlook.com.ar>
2024-04-25 22:42:05 -0300

    Lista de colores

--- src/main/java/org/tp1/view/Colores.java
+++ src/main/java/org/tp1/view/Colores.java
@@ -5,7 +5,18 @@ public enum Colores {
     RED("\u001B[31m"),
     GREEN("\u001B[32m"),
     YELLOW("\u001B[33m"),
-    BLUE("\u001B[34m");
+    BLUE("\u001B[34m"),
+    MAGENTA("\u001B[35m"),
+    CYAN("\u001B[36m"),
+    WHITE("\u001B[37m"),
+    BRIGHT_BLACK("\u001B[90m"),
+    BRIGHT_RED("\u001B[91m"),
+    BRIGHT_GREEN("\u001B[92m"),
+    BRIGHT_YELLOW("\u001B[93m"),
+    BRIGHT_BLUE("\u001B[94m"),
+    BRIGHT_MAGENTA("\u001B[95m"),
+    BRIGHT_CYAN("\u001B[96m"),
+    BRIGHT_WHITE("\u001B[97m");
 
     private final String color;
     Colores(String color){
